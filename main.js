26faab8073d0fd181c81108f18f736fc688cb609
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 19:45:07 +0100

    end snack-4

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ for (let i = 0; i < 6; i++) {
     const serial_number = Number(prompt('inserisci un numero'))
     if (serial_number % 2 !== 0) { 
         empty.push(serial_number)
-        } 
+        }
     }
+
+    
 console.log(empty);
