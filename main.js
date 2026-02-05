d87a99cee7b2ca8db24b1c87fbd010ff23d16023
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 12:51:39 +0100

    correction

--- main.js
+++ main.js
@@ -3,11 +3,8 @@ const empty = [];
 
 for (let i = 0; i < 6; i++) {
     const serial_number = Number(prompt('inserisci un numero'))
-    if (serial_number % 2 === 0) { 
-        console.log('nope');
-        } else {
-            empty.push(serial_number)
-        }
-}
-
+    if (serial_number % 2 !== 0) { 
+        empty.push(serial_number)
+        } 
+    }
 console.log(empty);
